fdeee60c563647c16dd6286723b43de24b8bb7e7
Caroline <caroline@bluerosedev.com>
2018-11-11 15:35:05 +0100

    Fix error resulting from rebase conflict

--- src/app/shared/http-interceptors/index.ts
+++ src/app/shared/http-interceptors/index.ts
@@ -1,6 +1,5 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import {HeadersInterceptor} from './headers-interceptor.service';
-import {ResponseInterceptor} from './reponse-interceptor.service';
 
 export const httpInterceptorProviders = [
   { provide: HTTP_INTERCEPTORS, useClass: HeadersInterceptor, multi: true }
